7ddb570bf15b7d27e8b541d8d3660c0b04538ca8
gitJackG <j.griffithsrico@gmail.com>
2025-11-30 20:05:08 +0000

    111 & 112 Easy added

---
+++ Easy/111_minimum_depth_of_binary_tree.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        return height(root);
+    }
+
+    private int height(TreeNode root) {
+        if (root == null)
+            return 0;
+        if (root.left == null)
+            return height(root.right) + 1;
+        if (root.right == null)
+            return height(root.left) + 1;
+        return Math.min(height(root.left), height(root.right)) + 1;
+    }
+}
