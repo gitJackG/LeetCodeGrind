3350427caa3d38ff5a2934b72e76eedfa6bac2dd
Jack <j.griffithsrico@gmail.com>
2025-12-16 13:23:24 +0000

    3 medium added java

---
+++ Medium/3_longest_substring_without_repeating_characters.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+        int j = 0;
+
+        HashSet<Character> visited = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++){
+            if (maxLength > (s.length() - i)) break;
+            while (visited.contains(s.charAt(i))){
+                visited.remove(s.charAt(j));
+                j++;
+            }
+            visited.add(s.charAt(i));
+            if (visited.size() > maxLength) maxLength = visited.size();
+        }
+        return maxLength;
+    }
+}
