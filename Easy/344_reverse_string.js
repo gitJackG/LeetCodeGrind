b850edb7796b6c6ffb6e920125534957551c2505
Jack Griffiths Rico <j.griffithsrico@gmail.com>
2026-02-20 10:45:05 +0000

    344 easy added js

---
+++ Easy/344_reverse_string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  var n = s.length;
+  for(let i = 0; i < n/2; i++){
+    let temp = s[i];
+    s[i] = s[n-i-1]; //null terminated
+    s[n-i-1] = temp;
+  }
+  return s;
+};
